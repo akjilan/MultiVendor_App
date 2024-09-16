29f5e22aeba017291672874da2f5054576421384
Raihanul Hoque Jilany <jilann.aj@gmail.com>
2024-09-17 02:00:57 +0600

    providers added

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  // console.log("hi")
+  return (
+    <div>
+      <h3 className="text-4xl bg-yellow-500 text-black">hello</h3>
+    </div>
+  );
+}
